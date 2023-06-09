3082843079f89bb26f88ef6bea6cd8a481e27cc3
anderson.silva <anderson.silva@ndd.tech>
2023-06-09 17:02:45 -0300

    redirecionar chamada para o host

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
+use std::fmt::format;
+use std::net::ToSocketAddrs;
+use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::TokioAsyncResolver;
 
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufReader};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{self, TcpListener, TcpStream};
 
 async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut http_data = Vec::new();
@@ -53,7 +58,45 @@ async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::err
 
         stream.write_all(response.as_bytes()).await?;
         stream.write_all(body_response.as_bytes()).await?;
+        stream.flush().await?;
     } else {
+        // let response = format!("HTTP/1.1 200 OK\r\n\r\n");
+        // stream.write_all(response.as_bytes()).await?;
+
+        // connect to the hos in the host headers
+        let mut host = String::new();
+        for h in req.headers.iter() {
+            if h.name.to_lowercase() == "host" {
+                host = String::from_utf8_lossy(h.value).to_string();
+                host.push('.');
+                break;
+            }
+        }
+        println!("host: {}", host);
+        let resolver =
+            TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+        let ips = resolver.lookup_ip(&host).await?;
+        println!("ips: {:?}", ips);
+        let ip = ips.iter().next().unwrap();
+
+        println!("ip: {}", ip.to_string());
+        let host_address = format!("{}:80", host);
+        let mut host_stream = TcpStream::connect(&host_address).await?;
+        host_stream.write_all(&http_data).await?;
+        host_stream.flush().await?;
+
+        let mut buffer = [0u8; 1024];
+
+        loop {
+            let n = host_stream.read(&mut buffer).await?;
+            println!("read {} bytes", n);
+            stream.write_all(&buffer[..n]).await?;
+            if n == 0 {
+                break;
+            }
+        }
+        println!("end");
+        stream.flush().await?;
     }
 
     Ok(())
